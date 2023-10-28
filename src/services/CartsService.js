ec1a5b471e669969e7f2ea78dd3d56c682e10eff
Pablo Franco <pabloeltano@hotmail.com>
2023-10-28 19:57:46 -0300

    Tercer PreEntrega

---
+++ src/services/CartsService.js
@@ -0,0 +1,22 @@
+export default class CartsService {
+  constructor(manager) {
+    this.manager = manager;
+  }
+
+  getCartById = (cid) => {
+    return this.manager.getCartById(cid);
+  };
+
+  createCart = () => {
+    return this.manager.createCart();
+  };
+
+  updateCart = (id, cart) => {
+    return this.manager.updateCart(id, cart);
+  };
+
+  deleteCart = () => {
+    return this.manager.deleteCart(id);
+  };
+}
+
